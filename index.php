06246cd8f02c52232af96c2ac4644195631734c2
Juliao96 <juliaogimi@gmail.com>
2023-01-20 09:55:45 +0100

    Create page admin

--- index.php
+++ index.php
@@ -4,20 +4,25 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Juliao\Page;
+use \Juliao\PageAdmin;
 
 $app = new Slim();
 
 //Este ponto vai permitir mostrar os erros. Ele vai mostrar no console.
 $app->config('debug', true);
 
-$app->get('/', function(){
-    //$sql = new Juliao\DB\Sql();
+$app->get('/', function(){ 
 
     $page = new Page();
 
     $page->setTpl("index");
+});
 
+$app->get('/admin', function(){
+
+    $page = new PageAdmin();
+    
+    $page->setTpl("index");
 });
 
 $app->run();
-?>
